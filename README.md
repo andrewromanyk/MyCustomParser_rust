81a4880c9b2e5d27536be8844f7a6899d641e0df
Andrii Romaniuk <andrewrom321@gmail.com>
2024-10-17 12:34:33 +0300

    first

---
+++ README.md
@@ -0,0 +1,11 @@
+### My parser
+
+My custom training parser
+
+![alt text](https://avatars.githubusercontent.com/u/5430905?s=200&v=4)
+
+### Example
+
+```rust
+very cool code
+```
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+//mod lib;
+use my_parser::*;
+
+pub fn main() {
+    assert_eq!(list_parser::list("[1,1,2,3,5,8]"), Ok(vec![1, 1, 2, 3, 5, 8]));
+    println!("{:?}", list_parser::list("[1,2,3,5,8]"));
+}
